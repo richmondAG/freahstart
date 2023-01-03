dc32328558ac1291bf7417c083ebbea0d06e6d9d
richie8989 <richmondsmith8989@gmail.com>
2023-01-03 03:44:13 -0500

    first

---
+++ Freshstart2/src/test/java/testcase/BaseTest.java
@@ -0,0 +1,40 @@
+package testcase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void basetest() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+
+		driver.manage().window().maximize();
+		
+		
+	}
+	
+	@AfterMethod
+	public void teardown() {
+		
+		driver.quit();
+		
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Freshstart2/src/test/java/testcase/Testcase1.java
@@ -0,0 +1,17 @@
+package testcase;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase1 extends BaseTest{
+	
+	@Test
+	public void test1() {
+	
+		String Title= driver.getTitle();
+		Assert.assertEquals(Title, "Google");
+		
+		
+	}
+	
+}
